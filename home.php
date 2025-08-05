8569128ec2f6dd46e6d0097240b67f964ac8b42a
Abinash Pattanayak <abinashpattanayak262@gmail.com>
2025-08-05 21:11:13 +0530

    added the fav icon

--- home.php
+++ home.php
@@ -11,7 +11,7 @@ $email = isset($_SESSION["email"]) ? $_SESSION["email"] : null;
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <title>MoBazaar - Home</title>
-  <link rel="icon" type="image/png" href="axisbank.png">
+  <link rel="icon" type="image/png" href="favlogo.jpeg">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
   <style>
     /* Reset & Base */
